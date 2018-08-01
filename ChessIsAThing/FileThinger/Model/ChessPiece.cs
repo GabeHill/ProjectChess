883c138f08075edb74e0dbb38aae63d20222875a
Richard Sanchez <superpoptart1@gmail.com>
2018-08-01 10:11:09 -0600

    Adding Model

---
+++ ChessIsAThing/FileThinger/Model/ChessPiece.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileThinger.Model
+{
+    class ChessPiece
+    {
+        Rank ranker;
+        Side sides;
+        XCoor xC;
+        YCoor yC;
+        public ChessPiece(Rank r, Side s, XCoor x, YCoor y)
+        {
+            this.ranker = r;
+            this.sides = s;
+            this.xC = x;
+            this.yC = y;
+        }
+
+        public XCoor XC { get => xC; }
+        public YCoor YC { get => yC; }
+        public Side Sides { get => sides; }
+        public Rank Ranker { get => ranker; }
+    }
+}
---
+++ ChessIsAThing/FileThinger/Model/Enums.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileThinger.Model
+
+{
+    public enum Rank
+    {
+        King = 'K',
+        Queen = 'Q',
+        Bishop = 'B',
+        Knight = 'N',
+        Rook = 'R',
+        Pawn = 'P'
+    }
+    public enum Side
+    {
+        light = 'l',
+        dark = 'd'
+    }
+    public enum XCoor
+    {
+        A = 'a',
+        B = 'b',
+        C = 'c',
+        D = 'd',
+        E = 'e',
+        F = 'f',
+        G = 'g',
+        H = 'h'
+    }
+    public enum YCoor
+    {
+        One = '1',
+        Two = '2',
+        Three = '3',
+        Four = '4',
+        Five = '5',
+        Six = '6',
+        Seven = '7',
+        Eight = '8'
+    }
+
+}
