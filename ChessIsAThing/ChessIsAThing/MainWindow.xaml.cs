d094be7079b27a807bdefea44513afc859bf98b8
h <h@DESKTOP-DG3A9RF>
2018-08-14 17:06:10 -0600

    artdoe

--- ChessIsAThing/ChessIsAThing/MainWindow.xaml.cs
+++ ChessIsAThing/ChessIsAThing/MainWindow.xaml.cs
@@ -13,16 +13,103 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ChessIsAThing
-{
+namespace ChessIsAThing {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+
+        private bool dragging;
+
+        private Point[][] points = new Point[][]{
+            new Point[8],
+            new Point[8],
+            new Point[8],
+            new Point[8],
+            new Point[8],
+            new Point[8],
+            new Point[8],
+            new Point[8]
+        };
+
+        public MainWindow() {
             InitializeComponent();
+
+            //Add colored rectangles to grid to create board
+            for(int i = 0; i<grdMain.RowDefinitions.Count-1; i++) {
+                for(int j = 0; j<grdMain.ColumnDefinitions.Count-1; j++) {
+
+                    Rectangle r = new Rectangle();
+                    if(i%2==0) {
+                        switch(j) {
+                            case 1:
+                            case 3:
+                            case 5:
+                            case 7:
+                                r.Fill=new SolidColorBrush(Colors.Black);
+                                break;
+                            default:
+                                r.Fill=new SolidColorBrush(Colors.White);
+                                break;
+                        }
+                    } else if(j%2==0) {
+                        r.Fill=new SolidColorBrush(Colors.Black);
+                    } else {
+                        r.Fill=new SolidColorBrush(Colors.White);
+                    }
+                    
+                    grdMain.Children.Add(r);
+                    Grid.SetColumn(r, j);
+                    Grid.SetRow(r, i);
+
+                    setBoard();
+
+                    points[i][j]=new Point(i,j);
+                }
+            }
+            
         }
+
+        private void setBoard() {
+
+
+            //r.MouseLeftButtonDown+=rectMouseButtonDown;
+            //r.MouseLeftButtonUp+=rectMouseButtonUp;
+            //r.MouseMove+=rectMouseMove;
+        }
+
+        private void cnvSnapToGrid(Rectangle r) {
+            int xPos = (int)(Canvas.GetTop(r)/100);
+            int yPos = (int)(Canvas.GetLeft(r)/100);
+
+            Canvas.SetTop(r,xPos*100);
+            Canvas.SetLeft(r, yPos*100);
+        }
+
+        private void rectMouseMove(object sender,MouseEventArgs e) {
+            if(!dragging) return;
+
+            var mousePos = e.GetPosition(grdMain);
+
+            double left = mousePos.X-(((Rectangle)sender).ActualWidth/2);
+            double top = mousePos.Y-(((Rectangle)sender).ActualHeight/2);
+            Canvas.SetLeft(((Rectangle)sender), left);
+            Canvas.SetTop(((Rectangle)sender), top);
+        }
+
+        private void rectMouseButtonDown(object sender, MouseButtonEventArgs e) {
+            dragging=true;
+            ((Rectangle)sender).CaptureMouse();
+        }
+
+        private void rectMouseButtonUp(object sender, MouseButtonEventArgs e) {
+            dragging=false;
+            ((Rectangle)sender).ReleaseMouseCapture();
+
+// TODO make snap to grid of stuff yeah idk
+
+        }
+
     }
 }
+
---
+++ ChessIsAThing/ChessIsAThing/models/Piece.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessIsAThing.models {
+
+    public enum Side {
+        White, Black
+    }
+
+    public enum Type {
+        Pawn, Queen, King, Bishop, Knight, Rook
+    }
+
+    public abstract class Piece {
+        public Side side { get; }
+        public bool MoveX { get; }
+        public bool MoveY { get; }
+        public bool MoveDiagonal { get; }
+        public bool SpecialCase { get; }
+
+        public Piece(Type t,Side s) {
+            side=s;
+            switch(t) {
+                case Type.Pawn:
+                    MoveX=true;
+                    MoveY=false;
+                    MoveDiagonal=false; SpecialCase=true;
+                    break;
+                case Type.Queen:
+                    MoveX=true;
+                    MoveY=true;
+                    MoveDiagonal=true;
+                    SpecialCase=false;
+                    break;
+                case Type.King:
+                    MoveX=true;
+                    MoveY=true;
+                    MoveDiagonal=true;
+                    SpecialCase=true;
+                    break;
+                case Type.Bishop:
+                    MoveX=false;
+                    MoveY=false;
+                    MoveDiagonal=true;
+                    SpecialCase=false;
+                    break;
+                case Type.Knight:
+                    MoveX=false;
+                    MoveY=false;
+                    MoveDiagonal=false;
+                    SpecialCase=true;
+                    break;
+                case Type.Rook:
+                    MoveX=true;
+                    MoveY=true;
+                    MoveDiagonal=false;
+                    SpecialCase=false;
+                    break;
+                default:
+                    MoveX=false;
+                    MoveY=false;
+                    MoveDiagonal=false;
+                    SpecialCase=false;
+                    break;
+            }
+
+        }
+
+        
+    }
+
+    public class Knight : Piece {
+        public Knight(Side s) : base(Type.Knight,s) { }
+    }
+
+    public class King : Piece {
+        public King(Side s) : base(Type.King,s) { }
+    }
+    public class Queen : Piece {
+        public Queen(Side s) : base(Type.Queen,s) { }
+    }
+    public class Pawn : Piece {
+        public Pawn(Side s) : base(Type.Pawn,s) { }
+    }
+    public class Bishop : Piece {
+        public Bishop(Side s) : base(Type.Bishop,s) { }
+    }
+}
